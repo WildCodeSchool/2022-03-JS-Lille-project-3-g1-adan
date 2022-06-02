88bcf884e99c0bb9cc30522f4c0e769322f015cd
laura <lauracourquin@outlook.com>
2022-06-02 10:59:00 +0200

    Linked index.jsx to style.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "@pages/Home";
+import Register from "@pages/Register";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Register />
       <Home />
       <p>coucou</p>
     </div>
--- frontend/src/pages/Register/index.jsx
+++ frontend/src/pages/Register/index.jsx
@@ -1,9 +1,11 @@
+import Logo from "@assets/Logo_ADAN.png";
 import SRegister from "./style";
 
 export default function Register() {
   return (
     <div>
       <SRegister>
+        <img src={Logo} alt="Logo ADAN" />
         <div className="register">
           <h1>Inscription</h1>
         </div>
