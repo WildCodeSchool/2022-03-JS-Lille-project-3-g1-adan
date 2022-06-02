c89fc8f35ed6634e48bf07bf3476e22a06889827
laura <lauracourquin@outlook.com>
2022-06-02 10:34:59 +0200

    Added page Register and added logo to assets

---
+++ frontend/src/pages/Register/index.jsx
@@ -0,0 +1,13 @@
+import SRegister from "./style";
+
+export default function Register() {
+  return (
+    <div>
+      <SRegister>
+        <div className="register">
+          <h1>Inscription</h1>
+        </div>
+      </SRegister>
+    </div>
+  );
+}
