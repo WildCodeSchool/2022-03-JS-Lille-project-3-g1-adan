720d7aa81d08e0450cb879c315f6798a72b1cd6a
laura <lauracourquin@outlook.com>
2022-06-03 15:08:22 +0200

    Added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Register from "@pages/Register";
+import RegisterArtist from "@pages/RegisterArtist";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/inscription" element={<Register />} />
+        <Route path="/inscription/artiste" element={<RegisterArtist />} />
       </Routes>
-      <Register />
     </div>
   );
 }
