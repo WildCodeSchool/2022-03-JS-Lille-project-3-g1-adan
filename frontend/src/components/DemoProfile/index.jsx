cb77cdbecec89b16b10741f61fe43a4154c92fb3
SDecambray <samir.decambray@gmail.com>
2022-06-09 10:01:28 +0200

    add and playable react player

--- frontend/src/components/DemoProfile/index.jsx
+++ frontend/src/components/DemoProfile/index.jsx
@@ -1,11 +1,19 @@
 import cover from "@assets/imgProfile/cover.jpg";
-// import ReactPlayer from "react-player";
+import ReactPlayer from "react-player";
 import SDemoProfile from "./style";
 
 function DemoProfile() {
   return (
     <SDemoProfile>
-      {/* <ReactPlayer url="https://soundcloud.com/miami-nights-1984/accelerated" /> */}
+      <div className="player-wrapper">
+        <ReactPlayer
+          url="https://youtu.be/F5tSoaJ93ac"
+          controls="true"
+          width="100%"
+          height="100%"
+          className="react-player"
+        />
+      </div>
       <img src={cover} alt="" />
     </SDemoProfile>
   );
--- frontend/src/components/DemoProfile/style.jsx
+++ frontend/src/components/DemoProfile/style.jsx
@@ -1,15 +1,13 @@
 import styled from "styled-components";
 
 export default styled.div`
-  margin: 0 2rem;
+  margin: 0 auto;
   padding: 1rem 0;
   width: 85vw;
   img {
     margin: 1rem;
     width: 30%;
   }
-  /* button {
-    color: black;
-  } */
+
   @media screen and (min-width: 400px);
 `;
