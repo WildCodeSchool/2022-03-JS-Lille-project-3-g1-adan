90be98de5505d51010058523b5ba00a9d94341b6
Decambray Samir <92584130+SDecambray@users.noreply.github.com>
2022-06-10 08:59:30 +0200

    Update SearchableManager.js

--- backend/src/models/SearchableManager.js
+++ backend/src/models/SearchableManager.js
@@ -9,20 +9,7 @@ class SearchableManager extends AbstractManager {
       [`%${search}%`]
     );
   }
-
-  // insert(item) {
-  //   return this.connection.query(
-  //     `insert into ${ItemManager.table} (title) values (?)`,
-  //     [item.title]
-  //   );
-  // }
-
-  // update(item) {
-  //   return this.connection.query(
-  //     `update ${ItemManager.table} set title = ? where id = ?`,
-  //     [item.title, item.id]
-  //   );
-  // }
+ }
 }
 
 module.exports = SearchableManager;
