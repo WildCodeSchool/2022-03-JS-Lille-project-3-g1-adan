01ea7ca1dbb6ac4501572daf007463b86e0cd2f4
Decambray Samir <92584130+SDecambray@users.noreply.github.com>
2022-06-13 09:28:05 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "@components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Profile from "@pages/Profile";
+import Home from "@pages/Home";
 
 function App() {
   return (
     <div className="App">
        <Navbar />
        <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/profile/:profileId" element={<Profile />} />
        </Routes>
      </div>
