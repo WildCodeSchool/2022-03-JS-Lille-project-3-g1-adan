4283ed0a1142b305ec109047de7bdd83d315c9b5
[Lille-JS] Valentin Nav <80594796+Elfaram@users.noreply.github.com>
2022-06-13 09:21:39 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,15 @@
 import Navbar from "@components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Profile from "@pages/Profile";
-import "./App.css";
 
 function App() {
   return (
-      <div className="App">
-      <Navbar />
-    <Routes>
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
-
+    <div className="App">
+       <Navbar />
+       <Routes>
+        <Route path="/profile" element={<Profile />} />
+       </Routes>
+     </div>
   );
 }
 
