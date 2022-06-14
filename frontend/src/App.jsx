3b57390c1b8a13de9aec2446aa446dbe3b032941
SDecambray <samir.decambray@gmail.com>
2022-06-14 15:40:57 +0200

    add main color to the backgroud

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,12 @@ import Home from "@pages/Home";
 function App() {
   return (
     <div className="App">
-       <Navbar />
-       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile/:profileId" element={<Profile />} />
-       </Routes>
-     </div>
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/:profileId" element={<Profile />} />
+      </Routes>
+    </div>
   );
 }
 
--- frontend/src/GlobalStyle.jsx
+++ frontend/src/GlobalStyle.jsx
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
 body {
   width: 100vw;
   height: 100vh;
+  background-color: var(--main-color)
 }
 `;
 export default GlobalStyle;
