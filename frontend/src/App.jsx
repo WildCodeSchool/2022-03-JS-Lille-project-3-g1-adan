3fa37b5113d7753bafdbcaa68066d51c399e48df
SDecambray <samir.decambray@gmail.com>
2022-06-15 14:47:19 +0200

    fixed css problem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,12 @@ import Home from "@pages/Home";
 function App() {
   return (
     <div className="App">
-       <Navbar />
-       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile/:profileId" element={<Profile />} />
-       </Routes>
-     </div>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/:profileId" element={<Profile />} />
+      </Routes>
+      <Navbar />
+    </div>
   );
 }
 
