7976515e5ca9e1b2beac6776fb5122e34d3e464b
Decambray Samir <92584130+SDecambray@users.noreply.github.com>
2022-06-15 09:56:00 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
 import Navbar from "@components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Profile from "@pages/Profile";
-
 import Search from "@pages/Search";
-
 import "./App.css";
-
 import Home from "@pages/Home";
 
 function App() {
