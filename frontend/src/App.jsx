0cea8ad12fe38050ffc86e55091b38ac99a5c62f
[Lille-JS] Valentin Nav <80594796+Elfaram@users.noreply.github.com>
2022-06-15 10:50:21 +0200

    Update App.jsx
    Deleted App.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import Navbar from "@components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Profile from "@pages/Profile";
-import "./App.css";
 import RegisterEmployer from "@components/RegisterEmployer";
 import Home from "@pages/Home";
 
