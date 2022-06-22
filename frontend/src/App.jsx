9eb165be894473ab4a29f92e945926bbf369319a
laura <lauracourquin@outlook.com>
2022-06-22 15:21:40 +0200

    Modified route /register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Search from "@pages/Search";
 import Home from "@pages/Home";
 import SignupArtist from "@components/SignupArtist";
 import RegisterArtist from "@components/RegisterArtistLast";
-import RegisterOne from "@components/RegisterOne";
+import Register from "@pages/Register";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Route path="/search" element={<Search />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/profile/:profileId" element={<Profile />} />
-        <Route path="/register" element={<RegisterOne />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/register/artist" element={<SignupArtist />} />
         <Route
           path="/register/artist/validation"
