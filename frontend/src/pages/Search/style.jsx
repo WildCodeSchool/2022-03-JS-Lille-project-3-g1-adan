288f8f1693e4c7558755ea133526103d56678ec8
camille <camille.arvieu@gmail.com>
2022-06-28 09:56:50 +0200

    refactoring the code to make the component more independant of the page

--- frontend/src/pages/Search/style.jsx
+++ frontend/src/pages/Search/style.jsx
@@ -14,38 +14,9 @@ export default styled.section`
     font-size: 1.2rem;
   }
 
-  ul {
-    background: #c4c4c4;
-    width: 90vw;
-    max-height: 80vh;
-    overflow: scroll;
-  }
-
-  li {
-    margin-top: 1rem;
-    margin-bottom: 0.5rem;
-    padding-left: 2vw;
-    display: flex;
-    flex-direction: column;
-    flex-flow: column wrap;
-  }
-
-  a {
-    color: black;
-    margin-top: 0.4rem;
-  }
   @media screen and (min-width: 500px) {
     height: 100vh;
-    ul {
-      width: 45vw;
-      height: fit-content;
-    }
-
-    li {
-      margin-top: 1rem;
-      margin-bottom: 0.8rem;
-    }
-
+    margin-top: 10vh;
     h2 {
       font-size: 3rem;
       margin: 4rem;
