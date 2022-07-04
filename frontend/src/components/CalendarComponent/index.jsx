881df06e0d488ce69f934581a441c9f4fa8c1db3
camille <camille.arvieu@gmail.com>
2022-07-04 09:50:58 +0200

    removing code from moment locale to let what solely necessary

--- frontend/src/components/CalendarComponent/index.jsx
+++ frontend/src/components/CalendarComponent/index.jsx
@@ -13,19 +13,8 @@ moment.locale("fr", {
     "janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre".split(
       "_"
     ),
-  monthsShort:
-    "janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.".split("_"),
-  monthsParseExact: true,
   weekdays: "Dimanche_Lundi_Mardi_Mercredi_Jeudi_Vendredi_Samedi".split("_"),
-  weekdaysShort: "dim._lun._mar._mer._jeu._ven._sam.".split("_"),
-  weekdaysMin: "Di_Lu_Ma_Me_Je_Ve_Sa".split("_"),
-  weekdaysParseExact: true,
   longDateFormat: {
-    LT: "HH:mm",
-    LTS: "HH:mm:ss",
-    L: "DD/MM/YYYY",
-    LL: "D MMMM YYYY",
-    LLL: "D MMMM YYYY HH:mm",
     LLLL: "dddd D MMMM YYYY",
   },
 });
