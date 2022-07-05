d6ccdce59c3179e70ecb240250da979fbdc8decb
[Lille-JS] Vaentin Nav <riverpure59@hotmail.fr>
2022-07-05 10:59:51 +0200

    modified code, better, fixed 2lign of CSS

--- frontend/src/components/RegisterEmployer/index.jsx
+++ frontend/src/components/RegisterEmployer/index.jsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Logo from "@assets/logo/Logo_ADAN.png";
 import SRegisterEmployer from "./style";
@@ -15,7 +14,10 @@ export default function RegisterEmployer() {
     companyStatus: "",
     numberSiret: "",
   });
-  const navigate = useNavigate();
+  const [currStep, setCurrStep] = useState(1);
+  const hNext = () => {
+    setCurrStep(Math.min(2, currStep + 1));
+  };
   const hChangeFormData = (evt) => {
     const newData = { ...formData };
     newData[evt.target.name] = evt.target.value;
@@ -31,9 +33,6 @@ export default function RegisterEmployer() {
           ...formData,
           user_id: id,
         });
-      })
-      .then(() => {
-        navigate("/employer");
       });
   };
 
@@ -41,80 +40,93 @@ export default function RegisterEmployer() {
     <SRegisterEmployer>
       <div className="register">
         <img src={Logo} className="imgLogo" alt="logo ADAN" />
-        <div className="registerContainer">
-          <h1>Organisateur</h1>
-          <form className="registerForm" onSubmit={hSubmit}>
-            <input
-              className="inputForm"
-              type="text"
-              name="lastname"
-              placeholder="Nom"
-              value={formData.lastname}
-              onChange={hChangeFormData}
-            />
-            <input
-              className="inputForm"
-              type="text"
-              name="firstname"
-              placeholder="Prénom"
-              value={formData.firstname}
-              onChange={hChangeFormData}
-            />
-            <input
-              className="inputForm"
-              type="text"
-              name="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={hChangeFormData}
-            />
-            <input
-              className="inputForm"
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={hChangeFormData}
-            />
-            <input
-              className="inputForm"
-              type="text"
-              name="numberSiret"
-              placeholder="N° SIRET"
-              value={formData.numberSiret}
-              onChange={hChangeFormData}
-            />
-            <input
-              className="inputForm"
-              type="text"
-              name="companyStatus"
-              placeholder="Catégorie juridique"
-              value={formData.companyStatus}
-              onChange={hChangeFormData}
-            />
-            <input
-              className="inputForm"
-              type="text"
-              name="companyName"
-              placeholder="Raison Sociale"
-              value={formData.companyName}
-              onChange={hChangeFormData}
-            />
-            <select
-              className="inputForm"
-              name="status"
-              onChange={hChangeFormData}
-            >
-              <option>Type d'employeur ...</option>
-              <option value="private">Privé </option>
-              <option value="public">Public </option>
-              <option value="personal">Personnel </option>
-            </select>
-            <button className="btnNext" type="submit" onChange={hSubmit}>
-              Validez
+        <fieldset className={currStep === 1 ? "active" : "inactive"}>
+          <div className="registerContainer">
+            <h1>Organisateur</h1>
+            <form className="registerForm" onSubmit={hSubmit}>
+              <input
+                className="inputForm"
+                type="text"
+                name="lastname"
+                placeholder="Nom"
+                value={formData.lastname}
+                onChange={hChangeFormData}
+              />
+              <input
+                className="inputForm"
+                type="text"
+                name="firstname"
+                placeholder="Prénom"
+                value={formData.firstname}
+                onChange={hChangeFormData}
+              />
+              <input
+                className="inputForm"
+                type="text"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={hChangeFormData}
+              />
+              <input
+                className="inputForm"
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={hChangeFormData}
+              />
+            </form>
+            <button className="btnNext" type="button" onClick={hNext}>
+              Suite
             </button>
-          </form>
-        </div>
+          </div>
+        </fieldset>
+
+        <fieldset className={currStep === 2 ? "active" : "inactive"}>
+          <div className="registerContainer">
+            <h1>Organisateur</h1>
+            <form className="registerForm" onSubmit={hSubmit}>
+              <input
+                className="inputForm"
+                type="text"
+                name="numberSiret"
+                placeholder="N° SIRET"
+                value={formData.numberSiret}
+                onChange={hChangeFormData}
+              />
+              <input
+                className="inputForm"
+                type="text"
+                name="companyStatus"
+                placeholder="Catégorie juridique"
+                value={formData.companyStatus}
+                onChange={hChangeFormData}
+              />
+              <input
+                className="inputForm"
+                type="text"
+                name="companyName"
+                placeholder="Raison Sociale"
+                value={formData.companyName}
+                onChange={hChangeFormData}
+              />
+              <select
+                className="inputForm"
+                name="status"
+                onChange={hChangeFormData}
+              >
+                <option>Type d'employeur ...</option>
+                <option value="private">Privé </option>
+                <option value="public">Public </option>
+                <option value="personal">Personnel </option>
+              </select>
+              <button className="btnNext" type="submit">
+                Validez
+              </button>
+            </form>
+          </div>
+        </fieldset>
       </div>
     </SRegisterEmployer>
   );
