776c60ac357253711f5193730d92cfbbc746d890
laura <lauracourquin@outlook.com>
2022-07-05 10:46:17 +0200

    Modified form and style.jsx

--- frontend/src/components/SignupArtist/index.jsx
+++ frontend/src/components/SignupArtist/index.jsx
@@ -13,6 +13,13 @@ export default function SignupArtist() {
     status: "",
     siren: "",
   });
+
+  const [currStep, setCurrStep] = useState(1);
+
+  const hNext = () => {
+    setCurrStep(Math.min(2, currStep + 1));
+  };
+
   const navigate = useNavigate();
   const hChangeFormData = (evt) => {
     const newData = { ...formData };
@@ -40,42 +47,54 @@ export default function SignupArtist() {
     <SSignupArtist>
       <div className="register">
         <img src={Logo} className="imgLogo" alt="Logo ADAN" />
-        <div className="registerContainer">
+        <fieldset className={currStep === 1 ? "active" : "inactive"}>
+          <div className="registerContainer">
+            <h1>Artiste</h1>
+            <form className="registerForm" onSubmit={hSubmit}>
+              <input
+                className="inputForm"
+                type="text"
+                name="lastname"
+                placeholder="Nom"
+                value={formData.lastname}
+                onChange={hChangeFormData}
+              />
+              <input
+                className="inputForm"
+                type="text"
+                name="firstname"
+                placeholder="Prénom"
+                value={formData.firstname}
+                onChange={hChangeFormData}
+              />
+
+              <input
+                className="inputForm"
+                type="text"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={hChangeFormData}
+              />
+              <input
+                className="inputForm"
+                type="password"
+                name="password"
+                placeholder="Mot de passe"
+                value={formData.password}
+                onChange={hChangeFormData}
+              />
+            </form>
+            <button className="btnNext" type="button" onClick={hNext}>
+              SUIVANT
+            </button>
+          </div>
+        </fieldset>
+      </div>
+      <fieldset className={currStep === 2 ? "active" : "inactive"}>
+        <div className="registerContainerTwo">
           <h1>Artiste</h1>
           <form className="registerForm" onSubmit={hSubmit}>
-            <input
-              className="inputForm"
-              type="text"
-              name="lastname"
-              placeholder="Nom"
-              value={formData.lastname}
-              onChange={hChangeFormData}
-            />
-            <input
-              className="inputForm"
-              type="text"
-              name="firstname"
-              placeholder="Prénom"
-              value={formData.firstname}
-              onChange={hChangeFormData}
-            />
-
-            <input
-              className="inputForm"
-              type="text"
-              name="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={hChangeFormData}
-            />
-            <input
-              className="inputForm"
-              type="password"
-              name="password"
-              placeholder="Mot de passe"
-              value={formData.password}
-              onChange={hChangeFormData}
-            />
             <select
               className="inputForm"
               name="status"
@@ -97,12 +116,12 @@ export default function SignupArtist() {
               value={formData.siren}
               onChange={hChangeFormData}
             />
-            <button className="btnNext" type="submit">
-              SUITE
+            <button className="btnValidate" type="submit">
+              VALIDEZ
             </button>
           </form>
         </div>
-      </div>
+      </fieldset>
     </SSignupArtist>
   );
 }
