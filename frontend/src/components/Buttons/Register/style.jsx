866b54b6f7866c08c642f8199edfad706e776e5c
MassimaMao <mastem@msn.com>
2022-07-13 16:05:23 +0200

    modify breakpoint 500px to 800px

--- frontend/src/components/Buttons/Register/style.jsx
+++ frontend/src/components/Buttons/Register/style.jsx
@@ -4,8 +4,10 @@ export default styled.button`
   width: 15rem;
   height: 2.2rem;
   border-radius: 1.6rem;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-size: 1rem;
 
-  @media screen and (min-width: 500px) {
+  @media screen and (min-width: 800px) {
     cursor: pointer;
     width: 26rem;
     height: 2rem;
