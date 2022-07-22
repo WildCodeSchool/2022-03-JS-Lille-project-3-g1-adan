b5e618b8798ceabd14d87a0eda16b8cff5e0e38d
LoicBrassart <loic.brassart@gmail.com>
2022-07-22 14:59:04 +0200

    Override default find function

--- backend/src/models/CalendarManager.js
+++ backend/src/models/CalendarManager.js
@@ -3,6 +3,13 @@ const AbstractManager = require("./AbstractManager");
 class CalendarManager extends AbstractManager {
   static table = "calendar";
 
+  find(id) {
+    return this.connection.query(
+      `select * from  ${this.table} where artist_id = ?`,
+      [id]
+    );
+  }
+
   insert(artistId, userId, dateEvent) {
     return this.connection.query(
       `insert into ${CalendarManager.table} ( artist_id, user_id, date_event) values (?,?,?)`,
