205af773e315ad98f51c12ef953eb60508fb4165
laura <lauracourquin@outlook.com>
2022-07-25 15:13:23 +0200

    Added route Profile mobile

--- frontend/src/components/NavMob/index.jsx
+++ frontend/src/components/NavMob/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import recherche from "@assets/pictureNavMob/Search.svg";
-import acceuil from "@assets/pictureNavMob/Home.svg";
+import accueil from "@assets/pictureNavMob/Home.svg";
 import contact from "@assets/pictureNavMob/contact.png";
 import profil from "@assets/pictureNavMob/Profil.svg";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import SNavMob from "./style";
 
 function NavMob() {
+  const { type } = useSelector((state) => state.user);
+  const { id } = useSelector((state) => state.user);
   return (
     <SNavMob>
       <nav className="mobile">
@@ -14,12 +17,12 @@ function NavMob() {
           <img src={recherche} alt="recherche" className="pict" />{" "}
         </Link>
         <Link to="/">
-          <img src={acceuil} alt="acceuil" className="pict" />
+          <img src={accueil} alt="accueil" className="pict" />
         </Link>
         <Link to="/message">
           <img src={contact} alt="contact" className="picto" />
         </Link>
-        <Link to="/artist/:profileId">
+        <Link to={`/${type}/${id}`}>
           <img src={profil} alt="profil" className="pict" />
         </Link>
       </nav>
