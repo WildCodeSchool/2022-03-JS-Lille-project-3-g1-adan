9802b3e9d6ce566a7efb5bd6d93de9204f4f89e9
camille <camille.arvieu@gmail.com>
2022-07-27 09:45:34 +0200

    fixing error in console on the Modal openning

--- frontend/src/components/CardProfileInfo/index.jsx
+++ frontend/src/components/CardProfileInfo/index.jsx
@@ -193,7 +193,7 @@ function CardProfileInfo() {
                 name="lastname"
                 id="lastname"
                 placeholder="Nom de famille"
-                value={formData.lastname}
+                value={formData.lastname || ""}
                 onChange={hChangeFormData}
               />
             </label>
@@ -206,7 +206,7 @@ function CardProfileInfo() {
               type="text"
               name="firstname"
               placeholder="Prénom"
-              value={formData.firstname}
+              value={formData.firstname || ""}
               onChange={hChangeFormData}
             />
           </label>
@@ -219,7 +219,7 @@ function CardProfileInfo() {
                 type="text"
                 name="city"
                 placeholder="Ville"
-                value={formData.city}
+                value={formData.city || ""}
                 onChange={hChangeFormData}
               />
             </label>
@@ -232,7 +232,7 @@ function CardProfileInfo() {
                 type="text"
                 name="avatar"
                 placeholder="Avatar"
-                value={formData.avatar}
+                value={formData.avatar || ""}
                 onChange={hChangeFormData}
               />
             </label>
@@ -245,7 +245,7 @@ function CardProfileInfo() {
                 type="text"
                 name="banner"
                 placeholder="Banner"
-                value={formData.banner}
+                value={formData.banner || ""}
                 onChange={hChangeFormData}
               />{" "}
             </label>
@@ -258,7 +258,7 @@ function CardProfileInfo() {
                 type="text"
                 name="demo"
                 placeholder="Démo"
-                value={formData.demo}
+                value={formData.demo || ""}
                 onChange={hChangeFormData}
               />
             </label>
@@ -271,7 +271,7 @@ function CardProfileInfo() {
                 type="text"
                 name="description"
                 placeholder="description"
-                value={formData.description}
+                value={formData.description || ""}
                 onChange={hChangeFormData}
               />
             </label>
@@ -284,7 +284,7 @@ function CardProfileInfo() {
                 type="text"
                 name="cachet"
                 placeholder="cachet"
-                value={formData.cachet}
+                value={formData.cachet || ""}
                 onChange={hChangeFormData}
               />
             </label>
@@ -297,7 +297,7 @@ function CardProfileInfo() {
                 type="text"
                 name="instagram"
                 placeholder="Instagram"
-                value={formData.instagram}
+                value={formData.instagram || ""}
                 onChange={hChangeFormData}
               />
             </label>
@@ -310,7 +310,7 @@ function CardProfileInfo() {
                 type="text"
                 name="linkedin"
                 placeholder="Linkedin"
-                value={formData.linkedin}
+                value={formData.linkedin || ""}
                 onChange={hChangeFormData}
               />
             </label>
@@ -323,7 +323,7 @@ function CardProfileInfo() {
                 type="text"
                 name="phone"
                 placeholder="Tel"
-                value={formData.phone}
+                value={formData.phone || ""}
                 onChange={hChangeFormData}
               />{" "}
             </label>
@@ -336,7 +336,7 @@ function CardProfileInfo() {
                 type="text"
                 name="adress"
                 placeholder="adresse"
-                value={formData.address}
+                value={formData.address || ""}
                 onChange={hChangeFormData}
               />
             </label>
@@ -349,7 +349,7 @@ function CardProfileInfo() {
                 type="text"
                 name="zipcode"
                 placeholder="Code postal"
-                value={formData.zipcode}
+                value={formData.zipcode || ""}
                 onChange={hChangeFormData}
               />{" "}
             </label>
@@ -362,7 +362,7 @@ function CardProfileInfo() {
                 type="text"
                 name="nickname"
                 placeholder="Pseudonyme"
-                value={formData.nickname}
+                value={formData.nickname || ""}
                 onChange={hChangeFormData}
               />
             </label>
@@ -375,7 +375,7 @@ function CardProfileInfo() {
                 type="text"
                 name="galleryImg"
                 placeholder="Galerie d'images"
-                value={formData.galleryImg}
+                value={formData.galleryImg || ""}
                 onChange={hChangeFormData}
               />
             </label>
@@ -388,7 +388,7 @@ function CardProfileInfo() {
                 type="text"
                 name="status"
                 placeholder="status"
-                value={formData.status}
+                value={formData.status || ""}
                 onChange={hChangeFormData}
               />
             </label>
@@ -401,7 +401,7 @@ function CardProfileInfo() {
                 type="text"
                 name="siren"
                 placeholder="siren"
-                value={formData.siren}
+                value={formData.siren || ""}
                 onChange={hChangeFormData}
               />
             </label>
